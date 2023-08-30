a0f5316b75275fa622981a485c217b15193c7f4e
Junior Lobo <jairraiollobo@gmail.com>
2023-08-30 01:37:02 -0300

    Conductor code update based on ticket ID conductor/subtask/a_subtask/2_1693370199987

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+src /
+  index.tsx```jsx
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const DEV_MODE = process.env.NODE_ENV !== 'production';
+
+const HookedComponent = () => {
+    useEffect(() => {
+        if (DEV_MODE) {
+            console.log('App-loaded');
+        }
+    }, []);
+
+    return (
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+};
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <HookedComponent />,
     document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+```;
